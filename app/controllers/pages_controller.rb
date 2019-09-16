1dd548adc1068660967da0977ea4b1c5497eb09f
Deorcan <tmlmqist@memphis.edu>
2019-09-16 17:23:42 -0500

    Commiting part 2 of HWIII

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,10 +4,23 @@ class PagesController < ApplicationController
           format.html { render :tmlmqist }
         end
     end
+    
+    
+    
+    
+    
+    
     def question
+      q="Which of the following is the Single Responsiblity Principle?"
+
+
+      answers=[ "a) A class should have only one reason to change.",
+      "b) Each developer on a team should have one main responsiblity.",
+      "c) Every app will have one responsiblity that is most important to users."
+      ]
+
       respond_to do |format|
-        format.html {render :question}
+        format.html {render :question, locals:{q:q,answers:answers}}
       end
     end
-    
 end    
